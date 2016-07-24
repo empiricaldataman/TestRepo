d4e6bb9fe7ff56578d128643859cec7543bf3662
Sal <cmelcocha-github@yahoo.com>
2016-07-23 21:46:12 -0500

    DBA
    Add DBA folder

---
+++ DBA/ServiceBrokerStatus.sql
@@ -0,0 +1,30 @@
+/*-----------------------------------------------------------------------------------------------
+        NAME: iServiceBroker_Status.sql
+ MODIFIED BY: Sal Young
+       EMAIL: saleyoun@yahoo.com
+ DESCRIPTION: Displays Service Broker information. It shows if the feature is enabled and if a 
+              db master key exist.
+-------------------------------------------------------------------------------------------------
+         DATE MODIFIED      DESCRIPTION    
+-------------------------------------------------------------------------------------------------
+   02.12.2012 SYoung        Initial creation.
+-------------------------------------------------------------------------------------------------
+  DISCLAIMER: The AUTHOR  ASSUMES NO RESPONSIBILITY  FOR ANYTHING, including  the destruction of 
+              personal property, creating singularities, making deep fried chicken, causing your 
+              toilet to  explode, making  your animals spin  around like mad, causing hair loss, 
+			        killing your buzz or ANYTHING else that can be thought up.
+-------------------------------------------------------------------------------------------------*/
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+SET NOCOUNT ON
+GO
+
+USE master
+GO
+
+SELECT name [DatabaseName]
+     , is_broker_enabled [IsServiceBrokerEnabled]
+     , is_master_key_encrypted_by_server [HasDatabaseMasterKey]
+  FROM sys.databases
+ WHERE is_broker_enabled = 1;
+GO
---
+++ DBA/TableDiffGen.sql
@@ -0,0 +1,23 @@
+/*-----------------------------------------------------------------------------------------------
+        NAME: iTableDiff_Gen.sql
+ MODIFIED BY: Sal Young
+       EMAIL: saleyoun@yahoo.com
+ DESCRIPTION: Generate a tablediff.exe command. The query window must be running in SQLCMD mode.
+-------------------------------------------------------------------------------------------------
+         DATE MODIFIED      DESCRIPTION    
+-------------------------------------------------------------------------------------------------
+   06.22.2015 SYoung        Initial creation.
+-------------------------------------------------------------------------------------------------
+  DISCLAIMER: The AUTHOR  ASSUMES NO RESPONSIBILITY  FOR ANYTHING, including  the destruction of 
+              personal property, creating singularities, making deep fried chicken, causing your 
+              toilet to  explode, making  your animals spin  around like mad, causing hair loss, 
+			        killing your buzz or ANYTHING else that can be thought up.
+-------------------------------------------------------------------------------------------------*/
+:setvar sourceserver "<source_server>"
+:setvar destinationserver "<target_server>"
+
+:setvar database "<db_name>"
+:setvar schema "<schema_name>"
+:setvar table "<table_name>"
+:!!tablediff.exe -sourceserver $(sourceserver) -sourcedatabase $(database) -sourceschema $(schema) -sourcetable $(table) -destinationserver $(destinationserver) -destinationdatabase $(database) -destinationschema $(schema) -destinationtable $(table) -c -f C:\temp\$(table).sql
+
